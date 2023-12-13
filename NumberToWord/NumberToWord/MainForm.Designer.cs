330b4b7804c8e672d3a4d20ca34626992daa85c1
Bakri Alloush <backar-net@hotmail.com>
2023-12-14 01:04:02 +0300

    Simplifying project

---
+++ NumberToWord/NumberToWord/MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace NumberToWord
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtArabicWord = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNumber.Location = new System.Drawing.Point(18, 25);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(457, 27);
+            this.txtNumber.TabIndex = 4;
+            this.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtArabicWord
+            // 
+            this.txtArabicWord.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArabicWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtArabicWord.Location = new System.Drawing.Point(18, 60);
+            this.txtArabicWord.Margin = new System.Windows.Forms.Padding(4);
+            this.txtArabicWord.Multiline = true;
+            this.txtArabicWord.Name = "txtArabicWord";
+            this.txtArabicWord.ReadOnly = true;
+            this.txtArabicWord.Size = new System.Drawing.Size(589, 157);
+            this.txtArabicWord.TabIndex = 6;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.btnConvert.Location = new System.Drawing.Point(482, 25);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(125, 27);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 230);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtArabicWord);
+            this.Controls.Add(this.txtNumber);
+            this.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number To Words";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtArabicWord;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ NumberToWord/NumberToWord/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NumberToWord
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtNumber.Text != "")
+                {
+                    txtArabicWord.Text = ToWord.ConvertToArabic(Convert.ToDecimal(txtNumber.Text));
+                }
+                else return;
+            }
+            catch (Exception ex)
+            {
+                txtArabicWord.Text = ex.Message;
+            }
+        }
+    }
+}
